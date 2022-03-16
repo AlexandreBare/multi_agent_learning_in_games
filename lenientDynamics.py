7e5a1217258a0ef21e3d1561a74ec88071909389
AlexandreBare <alexandre.bare@yahoo.fr>
2022-03-16 17:56:48 +0100

    Machine Learning Project: Task 2

---
+++ lenientDynamics.py
@@ -0,0 +1,120 @@
+import numpy as np
+from open_spiel.python.egt.dynamics import SinglePopulationDynamics
+from open_spiel.python.egt.dynamics import MultiPopulationDynamics
+
+
+def lenient_utility(tries, state, payoff_matrix):
+    utility = np.zeros(payoff_matrix.shape[0])
+    for i in range(payoff_matrix.shape[0]):
+        for j in range(payoff_matrix.shape[1]):
+            term1 = 0
+            for k in range(payoff_matrix.shape[1]):
+                if payoff_matrix[i, k] <= payoff_matrix[i, j]:
+                    term1 += state[k]
+            term1 **= tries
+
+            term2 = 0
+            for k in range(payoff_matrix.shape[1]):
+                if payoff_matrix[i, k] < payoff_matrix[i, j]:
+                    term2 += state[k]
+            term2 **= tries
+
+            term3 = 0
+            for k in range(payoff_matrix.shape[1]):
+                if payoff_matrix[i, k] == payoff_matrix[i, j]:
+                    term3 += state[k]
+
+            utility[i] += payoff_matrix[i, j] * state[j] * (term1 - term2) / term3
+    # print("Utility:\n", utility)
+    return utility
+
+
+class SinglePopulationLenientDynamics(SinglePopulationDynamics):
+    """Continuous-time single population lenient dynamics.
+      Attributes:
+        tries: The number of consecutive tries for each action from which the
+          rewards are collected before an update
+        payoff_matrix: The payoff matrix as an `numpy.ndarray` of shape `[2, k_1,
+          k_2]`, where `k_1` is the number of strategies of the first player and
+          `k_2` for the second player. The game is assumed to be symmetric.
+        dynamics: A callback function that returns the time-derivative of the
+          population state.
+
+    """
+
+    def __init__(self, tries, payoff_matrix, dynamics):
+        """Initializes the single-population dynamics."""
+        assert tries >= 1
+        super().__init__(payoff_matrix, dynamics)
+        self.tries = tries
+
+    def __call__(self, state=None, time=None):
+        """Time derivative of the population state.
+        Args:
+          state: Probability distribution as list or
+            `numpy.ndarray(shape=num_strategies)`.
+          time: Time is ignored (time-invariant dynamics). Including the argument in
+            the function signature supports numerical integration via e.g.
+            `scipy.integrate.odeint` which requires that the callback function has
+            at least two arguments (state and time).
+        Returns:
+          Time derivative of the population state as
+          `numpy.ndarray(shape=num_strategies)`.
+        """
+        state = np.array(state)
+        assert state.ndim == 1
+        assert state.shape[0] == self.payoff_matrix.shape[0]
+        # (Ax')' = xA'
+
+        utility = lenient_utility(self.tries, state, self.payoff_matrix)
+        # print(utility)
+        return self.dynamics(state, utility)
+
+
+class MultiPopulationLenientDynamics(MultiPopulationDynamics):
+    """Continuous-time multi-population lenient dynamics.
+    Attributes:
+    tries: The number of consecutive tries for each action from which the
+      rewards are collected before an update
+    payoff_tensor: The payoff tensor as an numpy.ndarray of size `[n, k0, k1,
+      k2, ...]`, where n is the number of players and `k0` is the number of
+      strategies of the first player, `k1` of the second player and so forth.
+    dynamics: List of callback functions for the time-derivative of the
+      population states, where `dynamics[i]` computes the time-derivative of the
+      i-th player's population state. If at construction, only a single callback
+      function is provided, the same function is used for all populations.
+    """
+
+    def __init__(self, tries, payoff_tensor, dynamics):
+        """Initializes the multi-population dynamics."""
+        assert tries >= 1
+        super().__init__(payoff_tensor, dynamics)
+        self.tries = tries
+
+    def __call__(self, state, time=None):
+        """Time derivative of the population states.
+        Args:
+          state: Combined population state for all populations as a list or flat
+            `numpy.ndarray` (ndim=1). Probability distributions are concatenated in
+            order of the players.
+          time: Time is ignored (time-invariant dynamics). Including the argument in
+            the function signature supports numerical integration via e.g.
+            `scipy.integrate.odeint` which requires that the callback function has
+            at least two arguments (state and time).
+        Returns:
+          Time derivative of the combined population state as `numpy.ndarray`.
+        """
+
+        state = np.array(state)
+        n = self.payoff_tensor.shape[0]  # number of players
+        ks = self.payoff_tensor.shape[1:]  # number of strategies for each player
+        assert state.shape[0] == sum(ks)
+
+        states = np.split(state, np.cumsum(ks)[:-1])
+        dstates = [None] * n
+        for i in range(n):
+            utility = lenient_utility(self.tries, states[i], self.payoff_tensor[i])
+
+            dstates[i] = self.dynamics[i](states[i], utility)
+
+        return np.concatenate(dstates)
---
+++ task2.py
@@ -0,0 +1,216 @@
+# Command-line arguments
+import argparse
+
+# Arrays
+import numpy as np
+
+# Visualization & Graphs
+from matplotlib import pyplot as plt
+
+# Openspiel
+import pyspiel
+from open_spiel.python import policy as policy_lib
+from open_spiel.python import rl_environment
+from open_spiel.python.algorithms import exploitability
+# from open_spiel.python.algorithms import random_agent
+from open_spiel.python.algorithms import tabular_qlearner
+from open_spiel.python.egt import dynamics
+from open_spiel.python.egt.utils import game_payoffs_array
+from open_spiel.python.egt import visualization
+
+import lenientDynamics
+
+OUTPUT_DIR = "outputs/"
+
+
+def load_game(game_name):
+    payoff_matrix_player_0 = None
+    payoff_matrix_player_1 = None
+
+    if game_name == 'Biased Rock Paper Scissors':
+        payoff_matrix_player_0 = [[0, -0.25, 0.5],
+                                  [0.25, 0, -0.05],
+                                  [-0.5, 0.05, 0]]
+        payoff_matrix_player_1 = [[0, 0.25, -0.5],
+                                  [-0.25, 0, 0.05],
+                                  [0.5, -0.05, 0]]
+        # payoff_matrix_player_1 = [[0, -0.25, 0.5],
+        #                           [0.25, 0, -0.05],
+        #                           [-0.5, 0.05, 0]]
+        actions = ['R', 'P', 'S']
+    elif game_name == 'Dispersion Game':
+        payoff_matrix_player_0 = [[-1, 1],
+                                  [1, -1]]
+        payoff_matrix_player_1 = [[-1, 1],
+                                  [1, -1]]
+        actions = ['A', 'B']
+    elif game_name == 'Battle of the Sexes':
+        payoff_matrix_player_0 = [[3, 0],
+                                  [0, 2]]
+        payoff_matrix_player_1 = [[2, 0],
+                                  [0, 3]]
+        actions = ['O', 'M']
+    elif game_name == 'Subsidy Game':
+        payoff_matrix_player_0 = [[10, 0],
+                                  [11, 12]]
+        payoff_matrix_player_1 = [[10, 11],
+                                  [0, 12]]
+        # payoff_matrix_player_1 = [[10, 0],
+        #                           [11, 12]]
+        actions = ['S1', 'S2']
+
+    game = pyspiel.create_matrix_game(game_name,
+                                      game_name,
+                                      actions,
+                                      actions,
+                                      payoff_matrix_player_0,
+                                      payoff_matrix_player_1)
+    game = pyspiel.convert_to_turn_based(game)
+    return game
+
+
+def train_agents(agents, env, training_episodes):
+    # Train the agents
+    for cur_episode in range(training_episodes):
+        time_step = env.reset()
+        while not time_step.last():
+            player_id = time_step.observations["current_player"]
+            agent_output = agents[player_id].step(time_step)
+            time_step = env.step([agent_output.action])
+        # Episode is over, step all agents with final info state.
+        for agent in agents:
+            agent.step(time_step)
+
+    return agents
+
+
+def plot_egt_dynamics(game,
+                      dynamics_func=dynamics.replicator,
+                      temperature=1.,
+                      tries=1,
+                      lenient=False,
+                      save_output=False):
+    payoff_tensor = game_payoffs_array(game)
+    game_name = game.get_parameters()['game']['name']
+    game_name_small = game_name.replace(' ', '_').lower()
+    dynamics_func_ = dynamics_func
+    if lenient:
+        dynamics_func_ = (lambda state_, utility_: dynamics_func(state_, utility_, temperature))
+
+    if game.num_distinct_actions() == 2:
+        if lenient:
+            dyn = lenientDynamics.MultiPopulationLenientDynamics(tries, payoff_tensor, dynamics_func_)
+        else:
+            dyn = dynamics.MultiPopulationDynamics(payoff_tensor, dynamics_func_)
+        projection = "2x2"
+        args_quiver = {"num_points": 12}
+        args_streamplot = {"num_points": 50, "linewidth": "velocity", "color": "velocity"}
+
+    elif game.num_distinct_actions() == 3:
+        if lenient:
+            dyn = lenientDynamics.SinglePopulationLenientDynamics(tries, payoff_tensor, dynamics_func_)
+        else:
+            dyn = dynamics.SinglePopulationDynamics(payoff_tensor, dynamics_func_)
+        projection = "3x3"
+        args_quiver = {}
+        args_streamplot = {"dt": 0.1, "density": 1.0, "min_length": 0.4, "linewidth": "velocity", "color": "velocity"}
+
+    state = game.new_initial_state()
+
+
+    fig = plt.figure()
+    ax = plt.subplot(projection=projection)
+    ax.quiver(dyn, **args_quiver)
+    if projection == "2x2":
+        plt.xlabel(state.action_to_string(0))
+        plt.ylabel(state.action_to_string(1))
+    else:
+        ax.set_labels([state.action_to_string(i) for i in range(game.num_distinct_actions())])
+
+    if lenient:
+        plt.title("Directional Field Plot for " + game_name + "\n(" + f"tries={tries}" + ", " + f"temperature={temperature}" + ")")
+    else:
+        plt.title("Directional Field Plot for " + game_name)
+    plt.show()
+
+    dir = OUTPUT_DIR + game_name_small + "/"
+    if save_output:
+        if lenient:
+            fig.savefig(dir + "directional_field_" + game_name_small + f"_tries={tries}" + "_" + f"temperature={temperature}" + ".png")
+        else:
+            fig.savefig(dir + "directional_field_" + game_name_small + ".png")
+
+
+    fig = plt.figure()
+    ax = plt.subplot(projection=projection)
+    ax.streamplot(dyn, **args_streamplot)
+    if projection == "2x2":
+        plt.xlabel(state.action_to_string(0))
+        plt.ylabel(state.action_to_string(1))
+    else:
+        ax.set_labels([state.action_to_string(i) for i in range(game.num_distinct_actions())])
+    if lenient:
+        plt.title("Trajectory for " + game_name + "\n(" + f"tries={tries}" + ", " + f"temperature={temperature}" + ")")
+    else:
+        plt.title("Trajectory for " + game_name)
+
+    plt.show()
+
+    if save_output:
+        if lenient:
+            fig.savefig(dir + "trajectory_" + game_name_small + f"_tries={tries}" + "_" + f"temperature={temperature}" + ".png")
+        else:
+            fig.savefig(dir + "trajectory_" + game_name_small + ".png")
+
+
+def main(args):
+    np.random.seed(42)
+    num_players = 2
+    game = load_game(args.game_name)
+    env = rl_environment.Environment(game)
+    num_actions = env.action_spec()["num_actions"]
+
+    # Initial state
+    policy = policy_lib.TabularPolicy(game)
+    print(policy.states_per_player)
+    print("Initial Action Probability Array:\n", policy.action_probability_array)
+    print("Nash Conv: ", exploitability.nash_conv(game, policy))
+    agents = [
+        tabular_qlearner.QLearner(player_id=player_id, num_actions=num_actions)
+        for player_id in range(num_players)
+    ]
+
+    # Training
+    training_episodes = 1000
+    agents = train_agents(agents, env, training_episodes)
+
+    # Play Time !
+    time_step = env.reset()
+    agent_output = agents[0].step(time_step, is_evaluation=True)
+    print("Agent 0: ", agent_output)
+    time_step = env.step([agent_output.action])
+    agent_output = agents[1].step(time_step, is_evaluation=True)
+    time_step = env.step([agent_output.action])
+    print("Agent 1: ", agent_output)
+
+    # Replicator Dynamics
+    plot_egt_dynamics(game, dynamics_func=dynamics.replicator, save_output=args.save_output)
+
+    # Lenient Boltzmann Q Dynamics
+    plot_egt_dynamics(game,
+                      dynamics_func=dynamics.boltzmannq,
+                      temperature=1.,
+                      tries=5,
+                      lenient=True,
+                      save_output=args.save_output)
+
+
+if __name__ == '__main__':
+    args_parser = argparse.ArgumentParser()
+    # 'Biased Rock Paper Scissors', 'Dispersion Game', 'Battle of the Sexes', 'Subsidy Game'
+    args_parser.add_argument('--game_name', help='Name of the game to play',
+                             default='Dispersion Game', type=str)
+    args_parser.add_argument('--save_output', help='Whether we save the output figures',
+                             default=True, type=bool)
+    args = args_parser.parse_args()
+    main(args)
